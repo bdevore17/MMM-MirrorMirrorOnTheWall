7a826bab424271be6f393cdcbf8e5532f2f0d72f
bdevore17 <bdevore17@gmail.com>
2017-07-23 21:21:31 -0500

    provisional torrent support

--- MMM-MirrorMirrorOnTheWall.js
+++ MMM-MirrorMirrorOnTheWall.js
@@ -3,45 +3,47 @@
 /* Magic Mirror
  * Module: MMM-MirrorMirrorOnTheWall
  *
- * 
+ *
  * MIT Licensed.
  */
 
 Module.register('MMM-MirrorMirrorOnTheWall', {
-
   defaults: {},
 
   start: function() {
     Log.info('Starting module: ' + this.name);
-    this.clear = false
+    this.clear = false;
     this.sendSocketNotification('ALEXA_START', {});
   },
 
   getStyles: function() {
     return [
-      "MMM-MirrorMirrorOnTheWall.css",
-      "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
-    ]
+      'MMM-MirrorMirrorOnTheWall.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css'
+    ];
   },
 
   // Override socket notification handler.
   socketNotificationReceived: function(notification, payload) {
-    Log.info(this.name + "received a socket notification:\n" + notification);
+    Log.info(this.name + 'received a socket notification:\n' + notification);
 
-    if (notification === "RESULT") {
-      this.clear = false
+    if (notification === 'RESULT') {
+      this.clear = false;
       this.result = payload;
-      this.updateDom()
+      this.updateDom();
       this.show(1000, function() {
         Log.log(this.name + ' is shown.');
       });
-    } else if (notification === "MODULE") {
-      let self = this
+    } else if (notification === 'MODULE') {
+      let self = this;
       MM.getModules().enumerate(function(module) {
-        if (module.name === payload.moduleName || payload.moduleName === "all_modules") {
+        if (
+          module.name === payload.moduleName ||
+          payload.moduleName === 'all_modules'
+        ) {
           if (payload.turnOn) {
             if (module.name === self.name) {
-              self.clear = false
+              self.clear = false;
               self.updateDom();
             }
             module.show(1000, function() {
@@ -49,7 +51,7 @@ Module.register('MMM-MirrorMirrorOnTheWall', {
             });
           } else {
             if (module.name === self.name) {
-              self.clear = true
+              self.clear = true;
               self.updateDom();
             }
             module.hide(1000, function() {
@@ -62,37 +64,47 @@ Module.register('MMM-MirrorMirrorOnTheWall', {
   },
 
   getDom: function() {
-    wrapper = document.createElement("div");
+    wrapper = document.createElement('div');
     wrapper.className = 'thin large bright';
 
     if (this.result && !this.clear) {
       if (this.result.images) {
-        var row = document.createElement("div")
-        row.className = "row"
+        var row = document.createElement('div');
+        row.className = 'row';
 
         for (var i = 0; i < this.result.images.length; i++) {
-          var img = document.createElement("img");
-          img.src = this.result.images[i].url
-          row.appendChild(img)
+          var img = document.createElement('img');
+          img.src = this.result.images[i].url;
+          row.appendChild(img);
         }
-        wrapper.appendChild(row)
+        wrapper.appendChild(row);
       }
 
       if (this.result.videoId) {
-        var videoWrapper = document.createElement("div")
-        videoWrapper.className = "videoWrapper"
-        var iframe = document.createElement('iframe')
-        iframe.src = "https://www.youtube.com/embed/" + this.result.videoId + "?autoplay=1&controls=0"
-        videoWrapper.appendChild(iframe)
-        wrapper.appendChild(videoWrapper)
+        var videoWrapper = document.createElement('div');
+        videoWrapper.className = 'videoWrapper';
+        var iframe = document.createElement('iframe');
+        iframe.src =
+          'https://www.youtube.com/embed/' +
+          this.result.videoId +
+          '?autoplay=1&controls=0';
+        videoWrapper.appendChild(iframe);
+        wrapper.appendChild(videoWrapper);
+      }
+
+      if (this.result.torrent) {
+        var torrentWrapper = document.createElement('div');
+        torrentWrapper.className = 'torrentWrapper';
+        torrentWrapper.innerHTML = `<video controls autoplay><source src="chargerinmotion.com:3000?name=${this.result.torrent}" type="video/mp4"/></video>`;
+        wrapper.appendChild(torrentWrapper);
       }
 
       if (this.result.displayText) {
-        var h1 = document.createElement('h1')
-        h1.className = "animated fadeIn"
-        var t = document.createTextNode(this.result.displayText)
-        h1.appendChild(t)
-        wrapper.appendChild(h1)
+        var h1 = document.createElement('h1');
+        h1.className = 'animated fadeIn';
+        var t = document.createTextNode(this.result.displayText);
+        h1.appendChild(t);
+        wrapper.appendChild(h1);
       }
     }
 
